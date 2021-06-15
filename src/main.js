02cd7dde0830ab94f58fbc1a68311240a8514109
MIAZ12 <1477560300@qq.com>
2021-06-16 00:35:12 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import fastClick from 'fastclick';
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import './assets/style/reset.css';
+import './assets/style/border.css';
+
 
 Vue.config.productionTip = false;
-
+fastClick.attach(document.body);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
@@ -13,3 +17,5 @@ new Vue({
   components: { App },
   template: '<App/>',
 });
+
+// 路由：根据网址的不同，返回不同的页面给用户
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Home from '../pages/home/home';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
+      // 访问根路径
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'Home',
+      component: Home,
     },
   ],
 });
