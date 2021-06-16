6cc2dfedd60cb12ffe7850d4c477fa2efa5c1fa7
MIAZ12 <1477560300@qq.com>
2021-06-17 00:04:35 +0800

    add Header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe658;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe63c;</span>输入城市
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont">&#xe65c;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeHeader",
+};
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  height: 0.86rem;
+  background: #00bcd4;
+  color: #fff;
+}
+.header-left {
+  width: 0.64rem;
+  float: left;
+}
+.header-left back-icon {
+  text-align: center;
+  font-size: 0.4rem;
+}
+.header-input {
+  margin-top: 0.12rem;
+  margin-bottom: 0.12rem;
+  margin-left: 0.2rem;
+  padding-left: 0.2rem;
+  flex: 1;
+  background: #fff;
+  border-radius: 0.1rem;
+  color: grey;
+}
+.header-right {
+  width: 1.24rem;
+  float: right;
+  text-align: center;
+}
+</style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    <div>home</div>
+    <div>
+      <home-header></home-header>
+    </div>
   </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header';
 export default {
   name: 'Home',
+  components:{
+    HomeHeader,
+  }
 };
 </script>
 
